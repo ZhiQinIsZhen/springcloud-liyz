17ab976883d0bfe994a991ac5e811bf10b564577
liyangzhen <liyangzhen@qjdchina.com>
2020-01-03 18:20:02 +0800

    11

--- service-member/member-web/src/main/java/com/liyz/cloud/service/member/controller/FeignUserInfoController.java
+++ service-member/member-web/src/main/java/com/liyz/cloud/service/member/controller/FeignUserInfoController.java
@@ -5,6 +5,7 @@ import com.liyz.cloud.service.member.remote.RemoteUserInfoService;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +24,7 @@ public class FeignUserInfoController {
     @Autowired
     RemoteUserInfoService remoteUserInfoService;
 
-    @GetMapping("/getByLoginName")
+    @PostMapping(value = "/getByLoginName", consumes = "application/json")
     public JwtUserBO getByLoginName(String loginName) {
         return remoteUserInfoService.getByLoginName(loginName);
     }
